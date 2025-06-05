0f3c3c306cadec541a75de94947b88ed54bbed9d
Hashane <jhashane@gmail.com>
2025-06-05 16:03:25 +0530

    Implementation: Authentication implemented using breeze.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,32 +1,36 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8" />
-    <meta name="description" content="Sharons Template" />
-    <meta name="keywords" content="Sharons, unica, creative, html" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>@yield('title', 'My App')</title>
-    @vite('resources/css/app.css')
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <!-- Google Font -->
-    <link
-        href="https://fonts.googleapis.com/css?family=Lora:400,700&display=swap"
-        rel="stylesheet"
-    />
-    <link
-        href="https://fonts.googleapis.com/css?family=Cabin:400,500,600,700&display=swap"
-        rel="stylesheet"
-    />
-</head>
-<body>
-@include('partials.header')
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
-<main>
-    @yield('content')
-</main>
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-@include('partials.footer')
-@vite('resources/js/app.js')
-</body>
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100">
+            @include('layouts.navigation')
+
+            <!-- Page Heading -->
+            @isset($header)
+                <header class="bg-white shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endisset
+
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
+    </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,46 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index');
-})->name('home');
-
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
-
-Route::get('/cart', function () {
-    return view('cart');
+    return view('welcome');
 });
 
-Route::get('/rooms', function () {
-    return view('rooms');
-})->name('rooms');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/room-details', function () {
-    return view('room-details');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/contact', function () {
-    return view('contact');
-})->name('contact');
+require __DIR__.'/auth.php';
+
+Route::middleware('guest')->group(function () {
+    Route::get('/', function () {
+        return view('index');
+    })->name('home');
+
+    Route::get('/about', function () {
+        return view('about');
+    })->name('about');
+
+    Route::get('/cart', function () {
+        return view('cart');
+    });
+
+    Route::get('/rooms', function () {
+        return view('rooms');
+    })->name('rooms');
+
+    Route::get('/room-details', function () {
+        return view('room-details');
+    });
+
+    Route::get('/contact', function () {
+        return view('contact');
+    })->name('contact');
+});
