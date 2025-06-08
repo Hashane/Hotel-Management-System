e089a87c3eeff8c1b4e43ec1afa922929af020da
Hashane <jhashane@gmail.com>
2025-06-09 00:20:11 +0530

    Bug-fix: Fixed room type filter issue which was caused when null is parsed when 'any' option is selected.

--- app/Utilities/RoomFilters/RoomType.php
+++ app/Utilities/RoomFilters/RoomType.php
@@ -4,11 +4,14 @@ namespace App\Utilities\RoomFilters;
 
 use App\Utilities\FilterContract;
 use App\Utilities\QueryFilter;
-
 class RoomType extends QueryFilter implements FilterContract
 {
     public function handle($value): void
     {
+        if ($value === 'any') {
+            return;
+        }
+
         $this->query
             ->join('room_types', 'rooms.room_type_id', '=', 'room_types.id')
             ->where('room_types.id', $value)
