23da2ba7eeaab9595a3475a83adb89945bd51ee6
Hashane <jhashane@gmail.com>
2025-06-13 03:14:39 +0530

    Enhancement: Filter form request validation for admin Room Reservation.

---
+++ app/Http/Requests/admin/FilterReservationRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests\admin;
+
+use App\Enums\RoomType;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class FilterReservationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'room_type' => ['nullable', Rule::enum(RoomType::class)],
+            'check_in' => ['nullable', 'date'],
+            'check_out' => ['nullable', 'date', 'after_or_equal:check_in'],
+            'occupants_count' => ['nullable', 'integer', 'min:1'],
+        ];
+    }
+
+    protected function prepareForValidation(): void
+    {
+        $this->merge(['check_in' => $this->check_in ?? now()->format('Y-m-d')]);
+    }
+}
