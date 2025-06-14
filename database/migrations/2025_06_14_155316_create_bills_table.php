8110c35288ad9fa8669828ceb62feedfacc4b554
Hashane <jhashane@gmail.com>
2025-06-14 21:24:54 +0530

    Enhancement: Added migrations and models for payments & bills.

---
+++ database/migrations/2025_06_14_155316_create_bills_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bills', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('reservation_id')->constrained()->cascadeOnDelete();
+            $table->decimal('subtotal', 10, 2);  // base + extras sum
+            $table->decimal('tax', 10, 2)->default(0);
+            $table->decimal('discount', 10, 2)->default(0);
+            $table->decimal('total', 10, 2); // subtotal + tax - discount
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bills');
+    }
+};
