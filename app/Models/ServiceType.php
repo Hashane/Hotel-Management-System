5192a542c2c4113835a283bfb6a89aa65df74b47
Hashane <jhashane@gmail.com>
2025-06-14 23:52:14 +0530

    Enhancement: Defined proper relationship between 'Multi valued attribute' ServiceType which belongs to the Extra charge entity.
    - As mentioned - https://stackoverflow.com/questions/55847487/how-does-laravel-handle-multivalued-attributes-in-table

--- app/Models/ServiceType.php
+++ app/Models/ServiceType.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ServiceType extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function ExtraCharges(): HasMany
+    {
+        return $this->hasMany(ExtraCharge::class);
+    }
 }
