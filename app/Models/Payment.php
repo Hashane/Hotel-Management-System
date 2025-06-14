cf8f424754a8945e9c7beac65f39161e6c517a28
Hashane <jhashane@gmail.com>
2025-06-14 23:11:56 +0530

    Implementation: Defined proper relationships related to billing and payments.

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     protected $guarded = [];
+
+    public function reservation()
+    {
+        return $this->belongsTo(Reservation::class);
+    }
+
+    public function bill()
+    {
+        return $this->belongsTo(Bill::class);
+    }
+
+    public function payments()
+    {
+        return $this->hasMany(Payment::class);
+    }
 }
