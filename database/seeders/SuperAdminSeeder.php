a4db0fde3557243135e855c5cfc1e26767ee5d4c
Hashane <jhashane@gmail.com>
2025-06-14 20:01:28 +0530

    Implementation: Super and Role seeders.

---
+++ database/seeders/SuperAdminSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+
+class SuperAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $role = Role::firstOrCreate(['name' => 'super admin']);
+
+        $user = User::firstOrCreate(
+            ['email' => 'admin@example.com'],
+            [
+                'name' => 'Super Admin',
+                'password' => Hash::make('password'),
+            ]
+        );
+
+        if (! $user->hasRole($role)) {
+            $user->assignRole($role);
+        }
+    }
+}
