5e80a9f525d9331c082eab1db05b384e2eae9151
Hashane <jhashane@gmail.com>
2025-06-16 09:25:14 +0530

    Revert: Redundant test code.

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,3 @@
 require __DIR__.'/auth.php';
 require __DIR__.'/admin.php';
 require __DIR__.'/customer.php';
-
-Route::get('/session-test', function () {
-    session(['test' => now()]);
-
-    return response()->json([
-        'id' => session()->getId(),
-        'test_value' => session('test'),
-        'cart' => session('cart', []),
-    ]);
-});
