0a32da65e9a0b149966ab308f0c6045bd907439f
Hashane <jhashane@gmail.com>
2025-06-16 09:08:58 +0530

    test routes

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,13 @@
 require __DIR__.'/auth.php';
 require __DIR__.'/admin.php';
 require __DIR__.'/customer.php';
+
+Route::get('/set-session', function () {
+    session(['test' => '123']);
+
+    return 'Session set!';
+});
+
+Route::get('/get-session', function () {
+    return session('test') ?? 'No session value';
+});
