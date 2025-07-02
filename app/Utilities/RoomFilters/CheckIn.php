730556565b9f7af11e3f1770b4dfdc1126b26332
Hashane <jhashane@gmail.com>
2025-07-03 03:00:09 +0530

    Enhancement: Reservation Filter logic enhanced to count available rooms and also to eager load rooms to display on Admin create reservations table.

--- app/Utilities/RoomFilters/CheckIn.php
+++ app/Utilities/RoomFilters/CheckIn.php
@@ -17,11 +17,34 @@ class CheckIn extends QueryFilter implements FilterContract
             return;
         }
 
-        $this->query->whereDoesntHave('rooms.roomReservations', function ($query) use ($checkIn, $checkOut) {
-            $query->where(function ($query) use ($checkIn, $checkOut) {
-                $query->where('check_in', '<', $checkOut)
-                    ->where('check_out', '>', $checkIn);
+        // Filter to only RoomTypes with at least one available room
+        $this->query->whereHas('rooms', function ($roomQuery) use ($checkIn, $checkOut) {
+            $roomQuery->whereDoesntHave('roomReservations', function ($resQuery) use ($checkIn, $checkOut) {
+                $resQuery->where(function ($q) use ($checkIn, $checkOut) {
+                    $q->where('check_in', '<', $checkOut)
+                        ->where('check_out', '>', $checkIn);
+                });
             });
         });
+
+        // Count the available rooms
+        $this->query->withCount(['rooms as available_rooms_count' => function ($roomQuery) use ($checkIn, $checkOut) {
+            $roomQuery->whereDoesntHave('roomReservations', function ($resQuery) use ($checkIn, $checkOut) {
+                $resQuery->where(function ($q) use ($checkIn, $checkOut) {
+                    $q->where('check_in', '<', $checkOut)
+                        ->where('check_out', '>', $checkIn);
+                });
+            });
+        }]);
+
+        // Eager-load only available rooms (in order to show them in  blade)
+        $this->query->with(['rooms' => function ($roomQuery) use ($checkIn, $checkOut) {
+            $roomQuery->whereDoesntHave('roomReservations', function ($resQuery) use ($checkIn, $checkOut) {
+                $resQuery->where(function ($q) use ($checkIn, $checkOut) {
+                    $q->where('check_in', '<', $checkOut)
+                        ->where('check_out', '>', $checkIn);
+                });
+            });
+        }]);
     }
 }
