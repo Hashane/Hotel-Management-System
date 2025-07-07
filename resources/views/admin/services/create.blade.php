b9873f99885933b085322053797af7b09d1e34b7
sheronchristeen <sheron200203@gmail.com>
2025-07-07 13:28:15 +0530

    Enhancements: made adjustments to prices section and added 'Price Calculation Method'

--- resources/views/admin/services/create.blade.php
+++ resources/views/admin/services/create.blade.php
@@ -107,56 +107,78 @@
               <div class="card-body">
                 <div class="row">
                   <div class="col-12">
-                    <!-- Add New Room Modal -->
                     <div class="container mt-2">
                       <form action="#" method="POST">
                         @csrf
 
+                        {{-- Pre Tax Operating Cost --}}
                         <div class="row mb-3">
-                          <div class="col-md-6 d-flex align-items-center">
-                            <label class="form-label me-3 mb-0" style="min-width: 180px;">Pre Tax Operating Cost</label>
-                            <input type="number" name="basic_price" class="form-control" required>
+                          <div class="col-md-4">
+                            <label class="form-label mb-0">Pre Tax Operating Cost</label>
+                          </div>
+                          <div class="col-md-4">
+                            <input type="number" name="operating_cost" class="form-control" required>
                           </div>
                         </div>
 
+                        {{-- Pre Tax Retail Price --}}
                         <div class="row mb-3">
-                          <div class="col-md-6 d-flex align-items-center">
-                            <label class="form-label me-3 mb-0" style="min-width: 180px;">Pre Tax Retail Price</label>
-                            <input type="number" name="basic_price" class="form-control" required>
+                          <div class="col-md-4">
+                            <label class="form-label mb-0">Pre Tax Retail Price</label>
+                          </div>
+                          <div class="col-md-4">
+                            <input type="number" name="retail_price" class="form-control" required>
                           </div>
                         </div>
 
+                        {{-- Tax Rule --}}
                         <div class="row mb-3">
-                          <div class="col-md-6 d-flex align-items-center">
-                            <label class="form-label me-3 mb-0" style="min-width: 180px;">Tax Rule</label>
-                            <input type="number" name="basic_price" class="form-control" required>
+                          <div class="col-md-4">
+                            <label class="form-label mb-0">Tax Rule</label>
+                          </div>
+                          <div class="col-md-4">
+                            <input type="number" name="tax_rule" class="form-control" required>
                           </div>
                         </div>
 
+                        {{-- Retail Price with Tax --}}
                         <div class="row mb-3">
-                          <div class="col-md-6 d-flex align-items-center">
-                            <label class="form-label me-3 mb-0" style="min-width: 180px;">Retail Price with Tax</label>
-                            <input type="number" name="basic_price" class="form-control" required>
+                          <div class="col-md-4">
+                            <label class="form-label mb-0">Retail Price with Tax</label>
+                          </div>
+                          <div class="col-md-4">
+                            <input type="number" name="final_price" class="form-control" required>
                           </div>
                         </div>
 
+                        {{-- Price Calculation Method --}}
+                        <div class="row mb-3">
+                          <div class="col-md-4">
+                            <label class="form-label mb-0">Price Calculation Method</label>
+                          </div>
+                          <div class="col-md-4">
+                            <select name="price_method" class="form-select">
+                              <option value="per_day">Per Day</option>
+                              <option value="per_booking">Per Booking</option>
+                            </select>
+                          </div>
+                        </div>
+
+                        {{-- Submit Buttons --}}
+                        <div class="row">
+                          <div class="col-12 text-end mt-4">
+                            <button type="submit" class="btn btn-primary">Save Room</button>
+                            <a href="#" class="btn btn-secondary">Cancel</a>
+                          </div>
+                        </div>
+
+                      </form>
                     </div>
-
-                    <div class="container mt-3">
-                      <div class="mt-4 text-end">
-                        <button type="submit" class="btn btn-primary">Save Room</button>
-                        <a href="#" class="btn btn-secondary">Cancel</a>
-                      </div>
-                    </div>
-                    </form>
                   </div>
                 </div>
-
-
               </div>
             </div>
           </div>
-
 
           {{-- *** SEO *** --}}
           <div class="tab-pane fade" id="vert-tabs-seo" role="tabpanel" aria-labelledby="vert-tabs-seo-tab">
