f96741b45bb1540c513bd17187666dedf9e19521
sheronchristeen <sheron200203@gmail.com>
2025-07-07 13:30:53 +0530

    Enhancements: added action colomn and removed 'add new feature' link

--- resources/views/admin/rooms/facilities.blade.php
+++ resources/views/admin/rooms/facilities.blade.php
@@ -39,8 +39,7 @@
       <div class="col-12">
         <div class="container mt-2">
           <div class="callout callout-info">
-            <div class="d-flex align-items-start ">
-
+            <div class="d-flex align-items-start">
               <i class="icon fas fa-info fa-lg text-info me-3 mt-1"></i>
               <div>
                 <h6 class="mb-0">
@@ -52,20 +51,13 @@
               </div>
             </div>
           </div>
-
         </div>
-
       </div>
     </div>
 
-
     <div class="row">
       <div class="col-12">
-
         <div class="container mt-2">
-
-          {{-- Paragraph --}}
-
           {{-- Feature Form --}}
           <form action="#" method="POST">
             @csrf
@@ -76,11 +68,14 @@
                 <div class="col-md-3 text-center">
                   <h6>Select</h6>
                 </div>
-                <div class="col-md-5">
+                <div class="col-md-4">
                   <h6>Feature</h6>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-3">
                   <h6>Logo</h6>
+                </div>
+                <div class="col-md-2 text-center">
+                  <h6>Action</h6>
                 </div>
               </div>
 
@@ -88,32 +83,43 @@
               $features = [
               ['name' => 'Wi‑Fi', 'image' => 'wifi.png'],
               ['name' => 'Fridge', 'image' => 'fridge.png'],
-              ['name' => 'Air Conditioner','image' => 'ac.png'],
+              ['name' => 'Air Conditioner', 'image' => 'ac.png'],
               ['name' => 'Television', 'image' => 'tv.png'],
               ['name' => 'Mini Bar', 'image' => 'minibar.png'],
               ];
               @endphp
             </div>
 
-
-
+            {{-- Feature Rows --}}
             <div class="container">
               @foreach ($features as $index => $feature)
               <div class="row align-items-center border-bottom mt-2 pb-2">
-                <div class="col-md-3 d-flex justify-content-center align-items-center ">
+                {{-- Checkbox --}}
+                <div class="col-md-3 d-flex justify-content-center align-items-center">
                   <input class="form-check-input" type="checkbox" name="features[]"
                     value="{{ strtolower(str_replace(' ', '_', $feature['name'])) }}" id="feature_{{ $index }}">
                 </div>
-                <div class="col-md-5">
+
+                {{-- Feature Name --}}
+                <div class="col-md-4">
                   <label class="form-check-label h6 mb-0" for="feature_{{ $index }}">
                     {{ $feature['name'] }}
                   </label>
                 </div>
-                <div class="col-md-4">
+
+                {{-- Feature Image --}}
+                <div class="col-md-3">
                   <h6 class="mb-0">
                     <img src="{{ asset('images/features/' . $feature['image']) }}" width="50"
                       alt="{{ $feature['name'] }}">
                   </h6>
+                </div>
+
+                {{-- Action Icon --}}
+                <div class="col-md-2 text-center">
+                  <i class="far fa-eye text-primary me-2" title="View"></i>
+                  <i class="far fa-edit text-success me-2" title="Edit"></i>
+                  <i class="far fa-trash-alt text-danger" title="Delete"></i>
                 </div>
               </div>
               @endforeach
@@ -121,29 +127,17 @@
           </form>
         </div>
 
-        {{-- + Add New Feature link (bottom-right) --}}
+        {{-- + Add New Feature link and Buttons --}}
         <div class="container mt-2">
-          <div class="row">
-            <div class="col-12 text-end">
-              <h6><a href="#"
-                  class="fw-bold text-primary text-decoration-none d-inline-flex align-items-center add-feature-link">
-                  <i class="fas fa-plus me-2"></i> Add New Feature
-                </a>
-              </h6>
 
-            </div>
-          </div>
 
           <div class="mt-4 text-end">
             <button type="submit" class="btn btn-primary">Save Room</button>
             <a href="#" class="btn btn-secondary">Cancel</a>
           </div>
         </div>
-
-
       </div>
     </div>
-
   </div>
 </div>
 
