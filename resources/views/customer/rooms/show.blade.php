c6a429275db3dcdf352a45b8a07818a5eb09ba99
sheronchristeen <sheron200203@gmail.com>
2025-07-09 22:57:49 +0530

    Enhancement: added Breadcrumb Section

--- resources/views/customer/rooms/show.blade.php
+++ resources/views/customer/rooms/show.blade.php
@@ -3,6 +3,25 @@
 @section('title', 'About Us')
 
 @section('content')
+<!-- Breadcrumb Section Begin -->
+<div class="breadcrumb-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="breadcrumb-text">
+                    <h2>Our Rooms</h2>
+                    <div class="bt-option">
+                        <a href="{{ route('home')}}">Home</a>
+                        <span>Rooms</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Breadcrumb Section End -->
+
+
 
 <body>
 
