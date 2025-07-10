28b32f02b220fe7611d7386ff2f6912fc4d1f579
sheronchristeen <sheron200203@gmail.com>
2025-07-10 17:11:53 +0530

    Enhancements

--- resources/views/customer/rooms/index.blade.php
+++ resources/views/customer/rooms/index.blade.php
@@ -25,38 +25,38 @@
 <!-- Rooms Section Begin -->
 <div class="container">
     <div class="row">
-        <div class="col-lg-2">
-            <div class="booking-form mb-5">
-                <h3>Booking Your Hotel</h3>
+
+
+        <!-- Booking section -->
+
+        <div class="col-lg-2 " style="padding: 0;">
+            <div class="booking-form" style="padding: 20px; border: 1px solid #ebebeb;">
+                <h3>Book Your Hotel</h3>
                 <form action="{{ route('rooms.index') }}" method="GET">
                     <div class="select-option">
-                        <label for="room_category">Accommodation Category:</label>
-                        <select id="room_category" name="room_category" class="r-o-select">
-                            @php
-                            $selectedRoomType = old('room_category', request('room_category', 'any'));
-                            @endphp
-                            <option value="any" {{ $selectedRoomType=='any' ? 'selected' : '' }}>Any</option>
-                            <option value="1" {{ $selectedRoomType=='1' ? 'selected' : '' }}>Standard</option>
-                            <option value="2" {{ $selectedRoomType=='2' ? 'selected' : '' }}>Deluxe</option>
-                            <option value="3" {{ $selectedRoomType=='3' ? 'selected' : '' }}>Suite</option>
+                        <label for="room_type">Accommodation Type:</label>
+                        <select id="room_type" name="room_type" class="r-o-select">
+                            <option value="any" {{ request('room_type')=='' ? 'selected' : '' }}>Any</option>
+                            <option value="1" {{ request('room_type')=='1' ? 'selected' : '' }}>Standard
+                            </option>
+                            <option value="2" {{ request('room_type')=='2' ? 'selected' : '' }}>Deluxe
+                            </option>
+                            <option value="3" {{ request('room_type')=='3' ? 'selected' : '' }}>Suite</option>
                         </select>
-                        <x-input-error :messages="$errors->get('room_category')" class="mt-2" />
                     </div>
 
                     <div class="check-date">
                         <label for="date-in">Check In:</label>
                         <input type="date" class="date-input" id="date-in" name="check_in"
-                            value="{{ old('check_in', request('check_in', now()->toDateString())) }}">
+                            value="{{ request()->check_in ?? now()->toDateString() }}">
                         <i class="icon_calendar"></i>
-                        <x-input-error :messages="$errors->get('check_in')" class="mt-2" />
                     </div>
 
                     <div class="check-date">
                         <label for="date-out">Check Out:</label>
                         <input type="date" class="date-input" id="date-out" name="check_out"
-                            value="{{ old('check_out', request('check_out', now()->toDateString())) }}">
+                            value="{{ request()->check_out ?? now()->addDay()->toDateString() }}">
                         <i class="icon_calendar"></i>
-                        <x-input-error :messages="$errors->get('check_out')" class="mt-2" />
                     </div>
 
                     <button type="submit">Check Availability</button>
@@ -64,13 +64,16 @@
             </div>
         </div>
 
-        <div class="col-lg-10">
+        <!-- Booking section -->
+
+        <div class="col-lg-10 ">
             @if(isset($message))
             <div class="alert alert-warning">{{ $message }}</div>
             @else
             @foreach ($roomTypes->chunk(3) as $roomChunk)
             <div class="row">
                 @foreach ($roomChunk as $roomType)
+
                 <div class="col-lg-4 mb-4">
                     <div class="room-item">
 
@@ -82,14 +85,14 @@
 
                             <div class="row">
                                 <div class="col-md-12">
-                                    <h4>{{ $roomType->name }} - {{ $roomType->available_rooms_count ?? 0 }}</h5>
-
+                                    <h4>{{ $roomType->name }} - {{ $roomType->available_rooms_count ?? 0
+                                        }}</h4>
                                 </div>
                             </div>
 
                             <div class="row mt-2 mb-2">
                                 <div class="col d-flex align-items-baseline">
-                                    <h4 class="mb-0 me-2">
+                                    <h4 class="mb-0 me-2 price">
                                         {{ $roomType->rateTypes[0]->pivot->price ?? 'N/A' }} $
                                     </h4>
                                     <h6 class="mb-0"><span>/Per night</span></h6>
@@ -97,49 +100,51 @@
                             </div>
 
                             <div class="row">
-                                <div class="col-md-12 d-flex align-items-center">
-                                    <p class="mb-0 me-2">Size:</p>
-                                    <p class="mb-0">
+                                <div class="col-md-12 d-flex align-items-center ">
+                                    <p class="mb-0 me-2 disc">Size:</p>
+                                    <p class="mb-0 disc">
                                         {{ $roomType->size ?? '30 ft' }}
                                     </p>
                                 </div>
 
                                 <div class="col-md-12 d-flex align-items-center">
-                                    <p class="mb-0 me-2">Capacity:
+                                    <p class="mb-0 me-2 disc">Capacity:
                                     </p>
-                                    <p class="mb-0" style="letter-spacing: 0.05em; line-height: 1.8;">
+                                    <p class="mb-0 disc" style="letter-spacing: 0.05em; line-height: 1.8;">
                                         Max person {{ $roomType->capacity ?? 2 }}
                                     </p>
                                 </div>
 
                                 <div class="col-md-12 d-flex align-items-center">
-                                    <p class="mb-0 me-2">Bed:</p>
-                                    <p class="mb-0" style="letter-spacing: 0.05em; line-height: 1.8;">
+                                    <p class="mb-0 me-2 disc">Bed:</p>
+                                    <p class="mb-0 disc" style="letter-spacing: 0.05em; line-height: 1.8;">
                                         {{ $roomType->bed_type ?? 'King Bed' }}
                                     </p>
                                 </div>
 
                                 <div class="col-md-12 d-flex align-items-start">
-                                    <p class="mb-0 me-2">
+                                    <p class="mb-0 me-2 disc">
                                         Facilities:
                                     </p>
-                                    <p class="mb-0" style="letter-spacing: 0.05em; line-height: 1.8;">
+                                    <p class="mb-0 disc" style="letter-spacing: 0.05em; line-height: 1.8;">
                                         {{
                                         $roomType->facilities()->pluck('name')->implode(', ') ??
                                         'Wifi, Television, Bathroom,...'
                                         }}
                                     </p>
                                 </div>
+
+                                <div class="col-md-12">
+                                    <a href="#" class="primary-btn mb-3 mt-3"
+                                        onclick="window.location='{{ route('rooms.show') }}'">More Details</a>
+                                </div>
                             </div>
 
 
 
 
 
-                            <div class="col-md-12">
-                                <a href="#" class="primary-btn mb-3 mt-3"
-                                    onclick="window.location='{{ route('rooms.show') }}'">More Details</a>
-                            </div>
+
 
                             <div class="row">
                                 <form action="{{ route('cart.add', ['roomType' => $roomType]) }}" method="POST"
